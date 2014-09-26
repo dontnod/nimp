0e4f82182368d0f8eb298cf86f2712f9fbf08b32
Sam Hocevar <sam@dont-nod.com>
2014-09-26 19:01:01 +0100

    Add a "cook everything" script.

---
+++ cook-everything.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import subprocess
+
+maps = [
+    "whatif.umap",
+    "whatif_menu.umap",
+    "e1_1a.umap",
+    #"e1_5a.umap",
+    #"e1_5b.umap",
+]
+
+args = [
+    "--noexpansion",
+]
+
+platforms = [
+    "PCConsole",
+    #"Win64",
+    "PS3",
+    "PS4",
+    "Xbox360",
+    "XboxOne",
+]
+
+configs = [
+    "release",
+    "test",
+]
+
+def ue3_cook(maps, args, platform, config):
+    print "Platform:", platform, "Config:", config
+    cmdline = "nimp.py --verbose ue3-cook"
+    cmdline += " " + " ".join(maps)
+    cmdline += " " + " ".join(args)
+    cmdline += " -p " + platform
+    cmdline += " -c " + config
+    print cmdline
+    subprocess.call(cmdline, shell = True)
+
+for p in platforms:
+    for c in configs:
+        ue3_cook(maps, args, p, c)
+
