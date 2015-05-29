d0a41ec117d7be436ba4440a3ed03745de6a0345
Corentin Séchet <corentin.sechet@dont-nod.com>
2015-05-29 15:17:24 +0200

    Added binaries deployment before loading packages in load-packages command

--- nimp/commands/cis_load_packages.py
+++ nimp/commands/cis_load_packages.py
@@ -27,5 +27,5 @@ class CisLoadPackagesCommand(CisCommand):
 
     #---------------------------------------------------------------------------
     def _cis_run(self, env):
-        #with deploy_latest_revision(env, env.publish_version, env.revision, ['Win64']):
-        return ue4_commandlet(env, "loadpackage", "-all")
+        with deploy_latest_revision(env, env.publish_version, env.revision, ['Win64']):
+            return ue4_commandlet(env, "loadpackage", "-all")
