7ac39e70226e05e01822f006bf781f7e0d6b8b65
Sam Hocevar <sam@dont-nod.com>
2015-06-03 14:32:37 +0200

    Use Visual Studio 2012 to build UE4 Durango binaries.

--- nimp/utilities/ue4.py
+++ nimp/utilities/ue4.py
@@ -12,6 +12,7 @@ import os
 from nimp.utilities.build import *
 from nimp.utilities.deployment import *
 
+
 #---------------------------------------------------------------------------
 def ue4_build(env):
     vs_version = '12'
@@ -30,24 +31,36 @@ def ue4_build(env):
             log_error("[nimp] Could not build PS4MapFileUtil.exe")
             return False
 
+    # The Durango XDK does not support Visual Studio 2013 yet
+    if env.is_xone:
+        vs_version = '11'
+
     return _ue4_build_project(env.solution, env.game, env.ue4_build_platform,
                               env.configuration, vs_version, 'Build')
 
-#---------------------------------------------------------------------------
-def _ue4_generate_project():
-    return call_process('.', ['../GenerateProjectFiles.bat'])
 
 #---------------------------------------------------------------------------
-def _ue4_build_project(sln_file, project, build_platform, configuration, vs_version, target = 'Rebuild'):
-    return vsbuild(sln_file, build_platform, configuration, project, vs_version, target)
+def _ue4_generate_project():
+
+    return call_process('.', ['../GenerateProjectFiles.bat'])
+
+
+#---------------------------------------------------------------------------
+def _ue4_build_project(sln_file, project, build_platform,
+                       configuration, vs_version, target = 'Rebuild'):
+
+    return vsbuild(sln_file, build_platform, configuration,
+                   project, vs_version, target)
+
 
 #---------------------------------------------------------------------------
 def ue4_commandlet(env, commandlet, *args):
     cmdline = [ "../Engine/Binaries/Win64/UE4Editor.exe",
-               env.game,
-               "-run=%s" % commandlet]
+                env.game,
+                "-run=%s" % commandlet]
 
     cmdline += list(args)
     cmdline += ['-nopause', '-buildmachine', '-forcelogflush', '-unattended', '-noscriptcheck']
 
     return call_process(".", cmdline) == 0
+
