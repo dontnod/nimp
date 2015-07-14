c61043a1596934efa036f1d27b37a5b4521e543e
Sam Hocevar <sam@dont-nod.com>
2015-07-15 00:37:25 +0200

    Minor local code refactoring.

--- nimp/modules/configuration.py
+++ nimp/modules/configuration.py
@@ -29,17 +29,18 @@ class ConfigurationModule(Module):
 def _load_settings(env):
     conf_dirs = []
     nimp_conf_dir = "."
+    nimp_conf_file = ".nimp.conf"
     while os.path.abspath(os.sep) != os.path.abspath(nimp_conf_dir):
-        if os.path.exists(os.path.join(nimp_conf_dir, ".nimp.conf")):
+        if os.path.exists(os.path.join(nimp_conf_dir, nimp_conf_file)):
             os.chdir(nimp_conf_dir)
             break
         nimp_conf_dir = os.path.join("..", nimp_conf_dir)
 
-    if not os.path.isfile(".nimp.conf"):
+    if not os.path.isfile(nimp_conf_file):
         return True
 
-    if not env.load_config_file(".nimp.conf"):
-        log_error(log_prefix() + "Error loading .nimp.conf.")
+    if not env.load_config_file(nimp_conf_file):
+        log_error(log_prefix() + "Error loading %s" % (nimp_conf_file))
         return False
 
     return True
