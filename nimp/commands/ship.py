faf5cd3780565d0fb7a896cf52a9c3303b26bc99
Sam Hocevar <sam@dont-nod.com>
2015-08-27 19:07:54 +0200

    Refactor the ship commandlet so that we can better split it later and make some steps optional.

--- nimp/commands/ship.py
+++ nimp/commands/ship.py
@@ -70,27 +70,107 @@ class ShipCommand(Command):
         # Unreal Engine 3
         if env.is_ue3:
 
-            # FIXME: maybe this should be handled inside ue3_ship() etc.
             loose_files_dir = env.format(env.loose_dir) if env.loose_dir else None
             pkgs_dir = env.format(env.pkg_dir) if env.pkg_dir else None
 
+            ship_game = (env.dlc == 'main')
+            ship_incremental = os.path.exists(env.format(env.publish_master))
+
             if not env.only_packages:
-                if env.dlc is not 'main':
+                log_notification(log_prefix() + "Performing UE3 ship… game:%d incremental:%d"
+                                                % (ship_game, ship_incremental))
+
+                if ship_incremental and not ship_game:
+                    log_error(log_prefix() + "Sorry, building a DLC patch is still not implemented")
+                    return False
+
+                # If cooking a DLC or a patch, we first need the original cook;
+                # either the master cook (when shipping a game patch) or one of
+                # the latest cooks done by buildbot
+                if ship_incremental or not ship_game:
                     log_notification(log_prefix() + "Deploying original cook…")
                     master_files = env.map_files()
-                    master_files.override(dlc = 'main', revision = 'HEAD').src(env.publish_cook).recursive().files()
+                    if ship_game:
+                        master_files_source = master_files.src(env.publish_master) \
+                                                          .recursive() \
+                                                          .files()
+                    else:
+                        master_files_source = master_files.override(dlc = 'main', revision = 'HEAD') \
+                                                          .src(env.publish_cook) \
+                                                          .recursive() \
+                                                          .files()
                     if not all_map(robocopy, master_files()):
                         return False
 
-                log_notification(log_prefix() + "Performing UE3 ship…")
-                if not ue3_ship(env, loose_files_dir):
+                # Now we can cook the desired maps
+                maps = env.cook_maps[env.dlc.lower()]
+
+                log_notification(log_prefix() + "Cooking maps: %s" % (" ".join(maps)))
+                if not ue3_cook(env.game,
+                                maps,
+                                env.languages,
+                                env.dlc,
+                                env.ue3_cook_platform,
+                                env.configuration,
+                                incremental = ship_game and ship_incremental):
                     return False
 
+                # If necessary, overwrite files we didn’t want to patch from the master again
+                if ship_game and ship_incremental:
+                        log_notification(log_prefix() + "Redeploying master cook ignoring patched files…")
+                        patch_files = env.map_files()
+                        patch_files.src(env.publish_master).override(step = 'patching').load_set("patch")
+                        files_to_exclude = [src for src, *args in patch_files()]
+                        log_notification("Excluding files %s", files_to_exclude)
+                        master_files_source.exclude_ignore_case(*files_to_exclude)
+                        if not file_mapper.all_map(robocopy, master_files()):
+                            return False
+
+            if not env.only_packages:
+                # Publish cooked directory to env.publish_cook
+                if ship_game and hasattr(env, 'revision'):
+                    cook_files = env.map_files()
+                    cook_files.to(env.publish_cook).load_set("cook")
+                    if not file_mapper.all_map(robocopy, cook_files()):
+                        return False
+
+            if not env.only_packages:
+                # Publish final files to env.publish_ship
+                destination = loose_files_dir or env.publish_ship
+
+                if ship_game:
+                    # PS3-specific step
+                    if env.is_ps3:
+                        ue3_generate_ps3_binaries(env)
+
+                    if not ship_incremental:
+                        # FIXME: implement this!
+                        log_warning(log_prefix() + "Publishing full game is not implemented")
+                    else:
+                        log_notification(log_prefix() + "Copying files to output directory…")
+                        patch_files = env.map_files()
+                        patch_files.to(destination).override(step = 'deploy').load_set("patch")
+                        if not file_mapper.all_map(robocopy, patch_files()):
+                            return False
+
+                        # Win32-specific step
+                        if env.is_win32:
+                            ue3_fix_pc_ini(env, destination)
+                else:
+                    log_notification(log_prefix() + "Copying DLC to output directory…")
+                    dlc_files = env.map_files()
+                    dlc_files.to(destination).load_set("dlc")
+
+                    if not file_mapper.all_map(robocopy, dlc_files()):
+                        return False
+
+            if not env.no_packages:
+                # Generate package configuration
                 log_notification(log_prefix() + "Generating package config…")
                 if not generate_pkg_config(env, loose_files_dir):
                     return False
 
-            if not env.no_packages:
+                # Generate packages
                 log_notification(log_prefix() + "Building packages…")
                 if not make_packages(env, loose_files_dir, pkgs_dir):
                     return False
@@ -98,6 +178,6 @@ class ShipCommand(Command):
             return True
 
         # Error!
-        log_error(log_prefix() + "Invalid project type {0}" % (env.project_type))
+        log_error(log_prefix() + "Invalid project type %s" % (env.project_type))
         return False
 
--- nimp/utilities/ue3.py
+++ nimp/utilities/ue3.py
@@ -155,101 +155,9 @@ def get_ue3_shader_platform(platform):
     return d[platform]
 
 
-#---------------------------------------------------------------------------
-def ue3_ship(env, destination = None):
-    master_directory = env.format(env.publish_master)
-
-    if os.path.exists(master_directory):
-        log_notification(log_prefix() + "Found a master at {0}: I’m going to build a patch", master_directory)
-        if env.dlc == 'main':
-            return _ship_game(env, destination or env.publish_ship, True)
-        else:
-            log_error(log_prefix() + "Sorry, building a DLC patch is still not implemented")
-    else:
-        if env.dlc == 'main':
-            return _ship_game(env, destination or env.publish_ship)
-        else:
-            return _ship_dlc(env, destination or env.publish_ship)
 
 #---------------------------------------------------------------------------
-def _ship_dlc(env, destination):
-    import nimp.utilities.file_mapper as file_mapper
-
-    maps = env.cook_maps[env.dlc.lower()]
-
-    log_notification(log_prefix() + "Cooking…")
-    if not ue3_cook(env.game,
-                    maps,
-                    env.languages,
-                    env.dlc,
-                    env.ue3_cook_platform,
-                    'final'):
-        return False
-
-    log_notification(log_prefix() + "Copying DLC to output directory…")
-    dlc_files = env.map_files()
-    dlc_files.to(destination).load_set("dlc")
-
-    return file_mapper.all_map(robocopy, dlc_files())
-
-#---------------------------------------------------------------------------
-def _ship_game(env, destination, incremental = False):
-    import nimp.utilities.file_mapper as file_mapper
-
-    maps = env.cook_maps[env.dlc.lower()]
-
-    if incremental:
-        master_files = env.map_files()
-        master_files_source = master_files.src(env.publish_master).recursive().files()
-        log_notification(log_prefix() + "Deploying master…")
-        if not file_mapper.all_map(robocopy, master_files()):
-            return False
-
-    log_notification(log_prefix() + "Cooking…")
-    if not ue3_cook(env.game,
-                    maps,
-                    env.languages,
-                    None,
-                    env.ue3_cook_platform,
-                    'final',
-                    incremental = incremental):
-        return False
-
-    if incremental:
-        log_notification(log_prefix() + "Redeploying master cook ignoring patched files…")
-        patch_files = env.map_files()
-        patch_files.src(env.publish_master).override(step = 'patching').load_set("patch")
-        files_to_exclude = [src for src, *args in patch_files()]
-        log_notification("Excluding files {0}", files_to_exclude)
-        master_files_source.exclude_ignore_case(*files_to_exclude)
-        if not file_mapper.all_map(robocopy, master_files()):
-            return False
-
-    if hasattr(env, 'revision'):
-        cook_files = env.map_files()
-        cook_files.to(env.publish_cook).load_set("cook")
-        if not file_mapper.all_map(robocopy, cook_files()):
-            return False
-
-    if env.is_ps3:
-        _generate_ps3_binaries(env)
-
-    log_notification(log_prefix() + "Copying files to output directory…")
-    if incremental:
-        patch_files = env.map_files()
-        patch_files.to(destination).override(step = 'deploy').load_set("patch")
-        if not file_mapper.all_map(robocopy, patch_files()):
-            return False
-
-        if env.is_win32:
-            _fix_pc_ini(env, destination)
-    else:
-        log_warning(log_prefix() + "Not implemented")
-
-    return True
-
-#---------------------------------------------------------------------------
-def _fix_pc_ini(env, destination):
+def ue3_fix_pc_ini(env, destination):
     destination = env.format(destination)
     base_game_ini_path = os.path.join(destination, "Engine/Config/BaseGame.ini")
     os.chmod(base_game_ini_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
@@ -259,7 +167,7 @@ def _fix_pc_ini(env, destination):
         base_game_ini.write(ini_content.replace("Example", "LifeIsStrange"))
 
 #---------------------------------------------------------------------------
-def _generate_ps3_binaries(env):
+def ue3_generate_ps3_binaries(env):
     for config in ["Shipping", "Test"]:
         if 0 != call_process(".", ["unfself",
                                     env.format("Binaries/PS3/{game}-PS3-%s.elf" % config),
@@ -307,7 +215,7 @@ def ue3_cook(game, maps, languages, dlc, platform, configuration, noexpansion = 
 
     commandlet_arguments += ['-multilanguagecook=' + '+'.join(languages), '-platform='+ platform ]
 
-    if dlc is not None:
+    if dlc is not 'main':
         commandlet_arguments += ["-dlcname={0}".format(dlc)]
 
     if noexpansion:
