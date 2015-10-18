4817fb2ff9efb9be73275662f2e851163faa07eb
Sam Hocevar <sam@dont-nod.com>
2015-10-18 17:26:44 +0200

    Add git_find() in addition to git_cat().

--- nimp/utilities/git.py
+++ nimp/utilities/git.py
@@ -13,3 +13,10 @@ def git_cat(path, repository, branch = 'master'):
                          shell = True, stdout=subprocess.PIPE)
     return p.stdout.read()
 
+
+# FIXME: this is rather inefficient since it transfers the whole tree instead of the filenames
+def git_find(path, repository, branch = 'master'):
+    p = subprocess.Popen('git archive --remote=%s %s %s | tar -tf -' % (repository, branch, path),
+                         shell = True, stdout=subprocess.PIPE)
+    return [l.strip().decode('utf-8') for l in p.stdout.readlines()]
+
