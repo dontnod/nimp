6eb7a9ccad70298570ce74981275ddd5dffd3ea4
Sam Hocevar <sam@dont-nod.com>
2016-03-14 15:11:08 +0100

    Disable nimp upgrade.

--- nimp/commands/upgrade.py
+++ nimp/commands/upgrade.py
@@ -5,27 +5,15 @@ import sys
 from nimp.utilities.processes import *
 from nimp.commands._command import *
 
-#-------------------------------------------------------------------------------
 class UpgradeCommand(Command):
-
     def __init__(self):
-        Command.__init__(self, 'upgrade', 'Upgrade Nimp')
+        Command.__init__(self, 'upgrade', 'Upgrade Nimp [DEPRECATED]')
 
-    #---------------------------------------------------------------------------
     def configure_arguments(self, env, parser):
         return True
 
-    #---------------------------------------------------------------------------
     def run(self, env):
-        cmdline = ['pip3', 'install', '--no-index', '--upgrade', '--no-deps', 'git+http://git/nimp.git']
-
-        return call_process(".", cmdline,
-                            stdout_callback = _stdout_callback,
-                            stderr_callback = _stderr_callback) == 0
-
-def _stdout_callback(line, default_log_function):
-    print(line, file = sys.stdout)
-
-def _stderr_callback(line, default_log_function):
-    print(line, file = sys.stderr)
+        log_error('Sorry, nimp upgrade is no longer supported. Please use:')
+        log_error('pip3 install --no-index --upgrade --no-deps git+http://git/nimp.git')
+        return False
 
