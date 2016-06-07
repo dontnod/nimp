8453bdc6eb6f5628120d95d9c4c4ced0aa99ff6e
Corentin Séchet <corentin.sechet@dont-nod.com>
2016-06-08 01:59:52 +0200

    Added fileset delete command

--- nimp/commands/fileset.py
+++ nimp/commands/fileset.py
@@ -21,19 +21,22 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ''' Fileset related commands '''
 
+import abc
+
 import logging
 
 import nimp.command
 import nimp.system
 
-class ListFileset(nimp.command.Command):
-    ''' Loads a fileset and prints mapped files '''
+class FilesetCommand(nimp.command.Command):
+    ''' Perforce command base class '''
+
     def __init__(self):
-        super(ListFileset, self).__init__()
+        super(FilesetCommand, self).__init__()
 
     def configure_arguments(self, env, parser):
         parser.add_argument('fileset',
-                            help    = 'Set name to load (e.g. binaries, version…)',
+                            help    = 'Set name to load (e.g. binaries, version...)',
                             metavar = '<fileset>')
 
         nimp.command.add_common_arguments(parser,
@@ -47,12 +50,43 @@ class ListFileset(nimp.command.Command):
         return True, ''
 
     def run(self, env):
-
         files = nimp.system.map_files(env)
         files_chain = files
         files_chain.load_set(env.fileset)
+        return self._run_fileset(files_chain)
 
-        for source, destination in files():
+    @abc.abstractmethod
+    def _run_fileset(self, file_mapper):
+        pass
+
+class Fileset(nimp.command.CommandGroup):
+    ''' Fileset related commands '''
+    def __init__(self):
+        super(Fileset, self).__init__([_List(),
+                                       _Delete()])
+
+    def is_available(self, env):
+        return True, ''
+
+class _Delete(FilesetCommand):
+    ''' Loads a fileset and delete mapped files '''
+    def __init__(self):
+        super(_Delete, self).__init__()
+
+    def _run_fileset(self, file_mapper):
+        for path, _ in file_mapper():
+            logging.info("Deleting %s", path)
+            nimp.system.force_delete(path)
+
+        return True
+
+class _List(FilesetCommand):
+    ''' Loads a fileset and prints mapped files '''
+    def __init__(self):
+        super(_List, self).__init__()
+
+    def _run_fileset(self, file_mapper):
+        for source, destination in file_mapper():
             logging.info("%s => %s", source, destination)
 
         return True
