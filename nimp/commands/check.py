5275a43a95a46368b39a427b6f2b9a3f3a55aeaa
Sam Hocevar <sam@dont-nod.com>
2016-10-10 17:17:13 +0200

    Improve the check commandlet
    “check processes” now accepts -k/--kill to explicitly kill processes,
    otherwise does nothing.
    
    “check disks” now has options for warnings, errors, and delayed exit.

--- nimp/commands/check.py
+++ nimp/commands/check.py
@@ -27,90 +27,48 @@ import platform
 import re
 import shutil
 import time
+import abc
 
 import nimp.command
 import nimp.system
 
-class Check(nimp.command.Command):
-    ''' Performs various checks on the environment '''
+class Check(nimp.command.CommandGroup):
+    ''' Check related commands '''
     def __init__(self):
-        super(Check, self).__init__()
+        super(Check, self).__init__([_Status(),
+                                     _Processes(),
+                                     _Disks()])
+    def is_available(self, env):
+        return True, ''
 
+
+class CheckCommand(nimp.command.Command):
+    ''' Performs various checks on the local host '''
+    def __init__(self):
+        super(CheckCommand, self).__init__()
 
     def configure_arguments(self, env, parser):
-        parser.add_argument('mode',
-                            help = 'Check mode (status, nimp, processes)',
-                            metavar = '<mode>',
-                            choices = ['status', 'processes', 'disk'])
-
         return True
 
     def is_available(self, env):
         return True, ''
 
     def run(self, env):
-        # Check running processes for issues and kill them if necessary
-        if env.mode == 'processes':
-            return Check._check_processes(env)
+        return self._run_check(env)
 
-        # Check disk space and block until there is free space
-        if env.mode == 'disk':
-            return Check._check_disk(env.root_dir)
+    @abc.abstractmethod
+    def _run_check(self,env):
+        pass
 
-        # Print information about the current environment
-        if env.mode == 'status':
-            Check._info_project(env)
-            Check._info_python()
-            Check._info_env()
-            return True
 
-        return False
+class _Status(CheckCommand):
+    def __init__(self):
+        super(_Status, self).__init__()
 
-    @staticmethod
-    def _check_processes(env):
-        logging.debug('Checking running processes…')
-
-        # Irrelevant on sane Unix platforms
-        if not nimp.system.is_windows():
-            return True
-
-        # Irrelevant if we’re not a UE3 or UE4 project
-        if not hasattr(env, 'project_type'):
-            return True
-
-        if env.project_type not in [ 'UE3', 'UE4' ]:
-            return True
-
-        # List all processes
-        cmd = ['wmic', 'process', 'get', 'executablepath,parentprocessid,processid', '/value']
-        result, output, _ = nimp.system.capture_process_output('.', cmd)
-        if result != 0:
-            return False
-
-        # Build a dictionary of all processes
-        processes = {}
-        path, pid, ppid = '', 0, 0
-        for line in [line.strip() for line in output.splitlines()]:
-            if line.lower().startswith('executablepath='):
-                path = re.sub('[^=]*=', '', line)
-            if line.lower().startswith('parentprocessid='):
-                ppid = re.sub('[^=]*=', '', line)
-            if line.lower().startswith('processid='):
-                pid = re.sub('[^=]*=', '', line)
-                processes[pid] = (path, ppid)
-
-        # Find all running binaries launched from the project directory and kill them
-        prefix = os.path.abspath(env.root_dir).replace('/', '\\').lower()
-        for pid, info in processes.items():
-            if info[0].lower().startswith(prefix):
-                logging.warning('Found problematic process %s (%s)', pid, info[0])
-                if info[1] in processes:
-                    logging.warning('Parent is %s (%s)', info[1], processes[info[1]][0])
-                logging.warning('Killing process…')
-                nimp.system.call_process('.', ['wmic', 'process', 'where', 'processid=' + pid, 'delete'])
-
-        logging.debug('%s processes checked.', len(processes))
-
+    def _run_check(self, env):
+        _Status._info_project(env)
+        _Status._info_python()
+        _Status._info_env()
         return True
 
     @staticmethod
@@ -148,22 +106,104 @@ class Check(nimp.command.Command):
             print('  ' + key + '=' + val)
         print()
 
-    @staticmethod
-    def _check_disk(path):
-        # Check every 5 minutes, for a maximum of 2 hours
-        wait_time = 5 * 60
-        total_wait_time = 2 * 60 * 60
-        byte2gib = 1.0 / 1024 / 1024 / 1024
-        while total_wait_time > 0:
+
+class _Processes(CheckCommand):
+    def __init__(self):
+        super(_Processes, self).__init__()
+
+    def configure_arguments(self, env, parser):
+        parser.add_argument('-k', '--kill',
+                            help = 'Kill processes that can prevent builds',
+                            default = False, action = 'store_true')
+        return True
+
+    def _run_check(self, env):
+        logging.info('Checking running processes…')
+
+        # Irrelevant on sane Unix platforms
+        if not nimp.system.is_windows():
+            return True
+
+        # Irrelevant if we’re not a UE4 project
+        if not hasattr(env, 'project_type') or env.project_type not in [ 'UE4' ]:
+            return True
+
+        # List all processes
+        cmd = ['wmic', 'process', 'get', 'executablepath,parentprocessid,processid', '/value']
+        result, output, _ = nimp.system.capture_process_output('.', cmd)
+        if result != 0:
+            return False
+
+        # Build a dictionary of all processes
+        processes = {}
+        path, pid, ppid = '', 0, 0
+        for line in [line.strip() for line in output.splitlines()]:
+            if line.lower().startswith('executablepath='):
+                path = re.sub('[^=]*=', '', line)
+            if line.lower().startswith('parentprocessid='):
+                ppid = re.sub('[^=]*=', '', line)
+            if line.lower().startswith('processid='):
+                pid = re.sub('[^=]*=', '', line)
+                processes[pid] = (path, ppid)
+
+        success = True
+
+        # Find all running binaries launched from the project directory and kill them
+        prefix = os.path.abspath(env.root_dir).replace('/', '\\').lower()
+        for pid, info in processes.items():
+            if info[0].lower().startswith(prefix):
+                logging.warning('Found problematic process %s (%s)', pid, info[0])
+                if info[1] in processes:
+                    logging.warning('Parent is %s (%s)', info[1], processes[info[1]][0])
+                if env.kill:
+                    logging.info('Killing process…')
+                    nimp.system.call_process('.', ['wmic', 'process', 'where', 'processid=' + pid, 'delete'])
+                else:
+                    success = False
+
+        logging.info('%s processes checked.', len(processes))
+
+        return success
+
+
+class _Disks(CheckCommand):
+    def __init__(self):
+        super(_Disks, self).__init__()
+
+    def configure_arguments(self, env, parser):
+        parser.add_argument('-w', '--warning',
+                            help = 'emit warnings when free space is below threshold (default 5.0)',
+                            metavar = '<percent>', type = float, default = 5.0)
+        parser.add_argument('-e', '--error',
+                            help = 'error out when free space is below threshold (default 1.0)',
+                            metavar = '<percent>', type = float, default = 1.0)
+        parser.add_argument('-d', '--delay',
+                            help = 'wait X seconds before exiting with error (default 10)',
+                            metavar = 'X', type = int, default = 10)
+        return True
+
+    def _run_check(self, env):
+        path = env.root_dir
+        wait_time = min(env.delay, 5 * 60) # Check at least every 5 minutes
+        total_wait_time = env.delay
+
+        while True:
             total, used, free = shutil.disk_usage(path)
-            logging.warning('Disk usage: %.2f GiB total, %.2f GiB used, %.2f GiB free',
-                            total * byte2gib, used * byte2gib, free * byte2gib)
-            free_percent = 100.0 * free / total
-            if free_percent > 0.5:
-                return True
-            logging.warning('Only %f%% free on disk, waiting for %d seconds',
-                            free_percent, wait_time)
+            byte2gib = 1.0 / 1024 / 1024 / 1024
+            byte2pct = 100.0 / total
+            logging.info('Disk usage: %.2f GiB total, %.2f GiB used (%.2f%%), %.2f GiB free (%.2f%%)',
+                         total * byte2gib, used * byte2gib, used * byte2pct, free * byte2gib, free * byte2pct)
+            free_percent = free * byte2pct
+            if free_percent < env.error:
+                if total_wait_time <= 0:
+                    return False
+                logging.warning('Only %.2f%% free on disk, waiting for %d seconds',
+                                free_percent, wait_time)
+            else:
+                if free_percent < env.warning:
+                    logging.warning('Only %.2f%% free space on disk', free_percent)
+                break
             time.sleep(wait_time)
             total_wait_time -= wait_time
-        return False
+        return True
 
