c4fc54bcef80ddbe0f9124cf7cf717e92398beab
Corentin Séchet <corentin.sechet@dont-nod.com>
2017-03-17 15:53:56 +0000

    Added unreal summary for sake of messages simplicity

--- nimp/unreal.py
+++ nimp/unreal.py
@@ -21,9 +21,10 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ''' Unreal Engine 4 related stuff '''
 
+import logging
 import os
 import platform
-import logging
+import re
 
 import nimp.build
 import nimp.system
@@ -353,7 +354,121 @@ def _ue4_load_arguments(env):
 
     return True
 
+def _cant_find_file(_, group_dict):
+    assert 'asset' in group_dict
+    asset_name = group_dict['asset']
+    msg_str = ('This asset contains a reference to %s which is missing.'
+               'You probably didn\'t fix references when deleting it, or '
+               'you forgot to add it to source control.')
+    return msg_str % asset_name
+
+_MESSAGE_PATTERNS = [
+    (re.compile(r'.*Can\'t find file.* \'\(?P<asset>[^\']\)\'.*'), _cant_find_file)
+]
+
+class _AssetSummary(object):
+    def __init__(self, hints, asset_name):
+        self._hints = hints
+        self._asset_name = asset_name
+        self._errors = set()
+        self._warnings = set()
+
+    def add_error(self, msg):
+        """ Adds a message to this asset's summary """
+        self._add_message(msg, self._errors)
+
+    def add_warning(self, msg):
+        """ Adds a message to this asset's summary """
+        self._add_message(msg, self._warnings)
+
+    def write(self, destination):
+        """ Writes summary for this asset into destination """
+        if len(self._errors) == 0 and len(self._warnings) == 0:
+            return
+        destination.write('%s :\n' % self._asset_name)
+        for error in self._errors:
+            destination.write(' * ERROR   : %s\n' % error)
+        for warning in self._warnings:
+            destination.write(' * WARNING : %s\n' % warning)
+
+    def _add_message(self, msg, destination):
+        for message_format, patterns in self._hints.items():
+            for pattern in patterns:
+                match = pattern.match(msg)
+                if match is not None:
+                    try:
+                        group_dict = match.groupdict()
+                        message = message_format.format(**group_dict)
+                        destination.add(message)
+                        return
+                    finally:
+                        pass
+
+        destination.add('Unknown error message : %s' % msg)
+
 class UnrealSummaryHandler(nimp.summary.SummaryHandler):
-    """ Summary handler presenting in a more cool way the results of load
-    package, load maps and cook commandlet """
-    pass
+    """ Default summary handler, showing one line by error / warning and
+    adding three lines of context before / after errors """
+    def __init__(self, env):
+        super().__init__(env)
+        self._summary = ''
+        self._asset_summaries = {}
+        self._hints = {}
+        self._load_asset_patterns = {}
+        self._current_asset = None
+        self._unknown_asset = _AssetSummary(self._hints, 'Unknown location')
+        load_asset_patterns = [
+            r'.*\[\d+\/\d+\] Loading [\.|\/]*(?P<asset>.*)\.\.\.$'
+        ]
+
+        self._load_asset_patterns = [
+            re.compile(it) for it in load_asset_patterns
+        ]
+
+        if hasattr(env, 'unreal_summary_hints'):
+            for message_format, patterns in env.unreal_summary_hints.items():
+                self._hints[message_format] = []
+                for pattern in patterns:
+                    try:
+                        self._hints[message_format].append(re.compile(pattern))
+                    #pylint: disable=broad-except
+                    except Exception as ex:
+                        logging.error('Error while compiling pattern %s: %s',
+                                      pattern, ex)
+
+    def _add_notif(self, msg):
+        self._update_current_asset(msg)
+
+    def _add_warning(self, msg):
+        current_asset = self._update_current_asset(msg)
+        current_asset.add_warning(msg)
+
+    def _add_error(self, msg):
+        current_asset = self._update_current_asset(msg)
+        current_asset.add_error(msg)
+
+    def _write_summary(self, destination):
+        ''' Writes summary to destination '''
+        for _, asset_summary in self._asset_summaries.items():
+            asset_summary.write(destination)
+
+        self._unknown_asset.write(destination)
+
+    def _update_current_asset(self, msg):
+        for pattern in self._load_asset_patterns:
+            match = pattern.match(msg)
+            if match is not None:
+                group_dict = match.groupdict()
+                assert 'asset' in group_dict
+                asset_name = group_dict['asset']
+                if asset_name not in self._asset_summaries:
+                    asset_summary = _AssetSummary(self._hints, asset_name)
+                    self._asset_summaries[asset_name] = asset_summary
+
+                self._current_asset = self._asset_summaries[asset_name]
+                return self._current_asset
+
+        if self._current_asset is not None:
+            return self._current_asset
+
+        return self._unknown_asset
