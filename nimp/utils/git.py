1578b669cff9238fd46d06b5ca8ea02b485f772a
Sam Hocevar <sam@dont-nod.com>
2019-02-04 11:25:31 +0100

    Fix bugs in Git utility functions:  - nimp.utils.git.get_branch() would fail if more than one branch were present  - nimp.utils.git.get_version() would fail to detect duplicate dates when    occurring at the same minute.

--- nimp/utils/git.py
+++ nimp/utils/git.py
@@ -24,14 +24,21 @@
 
 import nimp.sys.process
 
+
 def get_branch():
+    ''' Get the current active branch '''
     command = 'git branch --contains HEAD'
     result, output, _ = nimp.sys.process.call(command.split(' '), capture_output=True)
     if result != 0:
         return None
-    return output[2:].strip()
+    for l in output.splitlines():
+        if l[0:2] == '* ':
+            return l[2:].strip()
+    return None
+
 
 def get_version():
+    ''' Build a version string from the date and hash of the last commit '''
     from datetime import datetime, timezone
     command = 'git log -10 --date=short --pretty=format:%ct.%h'
     result, output, _ = nimp.sys.process.call(command.split(' '), capture_output=True)
@@ -40,13 +47,13 @@ def get_version():
     # Parse up to 10 revisions to detect date collisions
     n = 0
     for line in output.split('\n'):
-        pair = line.split('.')
+        new_date, new_shorthash = line.split('.')
+        utc_time = datetime.fromtimestamp(float(new_date), timezone.utc)
+        new_date = utc_time.astimezone().strftime("%Y%m%d.%H%M")
         if n == 0:
-            date, shorthash = pair
-        elif pair[0] != date:
+            date, shorthash = new_date, new_shorthash
+        elif new_date != date:
             break
         n += 1
-    utc_time = datetime.fromtimestamp(float(date), timezone.utc)
-    local_time = utc_time.astimezone()
-    return '.'.join([utc_time.astimezone().strftime("%Y%m%d.%H%M"), str(n), shorthash])
+    return '.'.join([date, str(n), shorthash])
 
