21efcb7c0793533ff7e4ed52f09573463f0fb1f0
Benjamin Hamon <benjamin.hamon@dont-nod.com>
2019-03-08 17:31:20 +0100

    Change distribution script to support python 3.5

--- scripts/configuration.py
+++ scripts/configuration.py
@@ -8,11 +8,8 @@ def load_configuration(environment):
         "distribution": "nimp-cli",
     }
 
-    revision = subprocess.run([ environment["git_executable"], "rev-parse", "--short=10", "HEAD" ], check = True, capture_output = True).stdout.decode("utf-8").strip()
-    branch = subprocess.run([ environment["git_executable"], "rev-parse", "--abbrev-ref", "HEAD" ], check = True, capture_output = True).stdout.decode("utf-8").strip()
-
-    configuration["project_version"]["revision"] = revision
-    configuration["project_version"]["branch"] = branch
+    configuration["project_version"]["revision"] = subprocess.check_output([ environment["git_executable"], "rev-parse", "--short=10", "HEAD" ]).decode("utf-8").strip()
+    configuration["project_version"]["branch"] = subprocess.check_output([ environment["git_executable"], "rev-parse", "--abbrev-ref", "HEAD" ]).decode("utf-8").strip()
     configuration["project_version"]["numeric"] = "{identifier}".format(**configuration["project_version"])
     configuration["project_version"]["full"] = "{identifier}+{revision}".format(**configuration["project_version"])
 
