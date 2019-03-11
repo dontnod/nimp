4fa862c53c72d706c4e3c27c52c6a9c9fbc3b362
Benjamin Hamon <benjamin.hamon@dont-nod.com>
2019-03-11 14:24:21 +0100

    Increment version to 0.11

--- scripts/configuration.py
+++ scripts/configuration.py
@@ -4,7 +4,7 @@ import subprocess
 def load_configuration(environment):
     configuration = {
         "project": "nimp",
-        "project_version": { "identifier": "0.10.0" },
+        "project_version": { "identifier": "0.11.0" },
         "distribution": "nimp-cli",
     }
 
