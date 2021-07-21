9658c6cd03b055cc26a2d5f2a57bc2b54f781bf0
Laurent Cahour <laurent.cahour@dont-nod.com>
2021-07-21 15:13:21 +0200

    Add PS5 and XSX to base platforms

---
+++ nimp/base_platforms/ps5.py
@@ -0,0 +1,145 @@
+
+import glob
+import json
+import logging
+import os
+import re
+import subprocess
+
+import nimp.sys.platform
+
+class PS5(nimp.sys.platform.Platform):
+    ''' PS5 platform description '''
+
+    def __init__(self):
+        super().__init__()
+
+        self.name = 'ps5'
+        #self.aliases = set(['prospero', 'dpx'])
+        self.aliases = set(['prospero'])
+        self.is_sony = True
+
+        self.layout_file_extension = 'gp5'
+        if not os.getenv('UE_SDKS_ROOT'):
+            self.package_tool_path = os.path.join(os.getenv('SCE_ROOT_DIR', default='.'), 'PROSPERO', 'Tools', 'Publishing Tools', 'bin', 'prospero-pub-cmd.exe')
+
+        self.unreal_name = 'PS5'
+        self.unreal_config_name = 'PS5'
+        self.unreal_cook_name = 'PS5'
+        self.ue4_package_directory = '{uproject_dir}/Binaries/PS5'
+
+    def install_package(self, package_directory, env):
+        pkgs = glob.glob(package_directory + '/*' + env.ue4_config + '.pkg')
+        if not pkgs:
+            raise RuntimeError('No pkg file for configuration ' + env.ue4_config + ' in ' + package_directory)
+        if len(pkgs) != 1:
+            logging.info('Found pkgs:')
+            for pkg in pkgs:
+                logging.info('\t\t' + pkg)
+            raise RuntimeError('Multiple pkg files in ' + package_directory + ' for configuration ' + env.ue4_config)
+
+        return PS5.prospero_ctrl([ 'package', 'install', pkgs[0] ], env.device, dry_run=env.dry_run)
+
+    def launch_package(self, package_name, env):
+        if not package_name:
+            package_name = env.game
+            title_id = self.get_title_id_from_json(env.uproject_dir)
+
+        if not title_id:
+            installed_titles = self.get_installed_titles(env.device)
+            title_id = self.pick_title_id(installed_titles, package_name)
+
+        return PS5.prospero_run([ '-app', title_id ], env.device, dry_run=env.dry_run)
+
+    _TITLE_ID_RE = re.compile(r'- TitleId: (.*)')
+    _TITLE_NAME_RE = re.compile(r'  TitleName: (.*)')
+    def get_installed_titles(self, device_ip):
+        cmdline = PS5.PROSPERO_CTRL + ' package list'
+        if device_ip:
+            cmdline += ' /target:' + device_ip
+        status, output = subprocess.getstatusoutput(cmdline)
+        if status != 0:
+            raise RuntimeError('Failed to get list of packages')
+        
+        last_title_id = None
+        installed_titles = {}
+        for line in output.split('\n'):
+            m = PS5._TITLE_ID_RE.match(line)
+            if m:
+                last_title_id = m.group(1)
+                continue
+
+            m = PS5._TITLE_NAME_RE.match(line)
+            if not m:
+                continue
+
+            title_name = m.group(1)
+            if not last_title_id:
+                raise RuntimeError('Parsing error: missing TitleId before TitleName')
+            installed_titles[title_name] = last_title_id
+            last_title_id = None
+
+        return installed_titles
+    
+    def pick_title_id(self, installed_titles, package_name):
+        matching_title_ids = []
+        for title_name, title_id in installed_titles.items():
+            if package_name in title_name:
+                matching_title_ids.append(title_id)
+
+        if len(matching_title_ids) == 1:
+            return matching_title_ids[0]
+
+        logging.info("Installed packages:")
+        for title_name in installed_titles.keys():
+            logging.info("\t\t" + title_name)
+        if not matching_title_ids:
+            raise RuntimeError('Package ' + package_name + ' not found')
+        else:
+            raise RuntimeError('Multiple packages found for ' + package_name)
+
+    _CONTENT_ID_RE = re.compile(r'[A-Z]{2}[0-9]{4}-([A-Z]{4}[0-9]{5})_00-[A-Z0-9]{16}')
+    def get_title_id_from_json(self, project_directory):
+        json_file_path = project_directory + '/Platforms/PS5/Build/TitleConfiguration.json'
+        if not os.path.exists(json_file_path):
+            return None
+        with open(json_file_path) as json_file:
+            json_content = json.load(json_file)
+        if not 'DefaultContentID' in json_content:
+            logging.warning('No "DefaultContentID" attribute found in ' + json_file_path + '. Looking for package name instead.')
+            return None
+
+        content_id = json_content['DefaultContentID'] # Use the same content ID as UE4's PS5TitleConfig.cs
+        m = PS5._CONTENT_ID_RE.match(content_id)
+        if not m:
+            logging.warning('"DefaultContentID" attribute in ' + json_file_path + ' does not match the expected format. Looking for package name instaed.')
+            return None
+
+        return m.group(1)
+
+    def find_sdk():
+        # if os.getenv('UE_SDKS_ROOT'):
+        #     raise RuntimeError('You seem to be using AutoSDK, which is not supported yet')
+        sdk = os.getenv('SCE_PROSPERO_SDK_DIR')
+        if sdk:
+            return sdk
+        return 'C:\\Program Files (x86)\\SCE\\Prospero SDKs\\2.000'
+
+    SDK = find_sdk()
+    TOOLS_BIN_DIR = SDK + '\\..\\..\\Prospero\\Tools\\Target Manager Server\\bin'
+    PROSPERO_CTRL = TOOLS_BIN_DIR + '\\prospero-ctrl.exe'
+    PROSPERO_RUN = TOOLS_BIN_DIR + '\\prospero-run.exe'
+
+    @staticmethod
+    def prospero_ctrl(args, ip=None, dry_run=False):
+        if ip:
+            args.append('/target:' + ip)
+        result = nimp.sys.process.call([PS5.PROSPERO_CTRL] + args, dry_run=dry_run)
+        return result == 0
+
+    @staticmethod
+    def prospero_run(args, ip=None, dry_run=False):
+        if ip:
+            args = ['/target:' + ip] + args # "/target" needs to be before "/app"
+        result = nimp.sys.process.call([PS5.PROSPERO_RUN] + args, dry_run=dry_run)
+        return result == 0
---
+++ nimp/base_platforms/xsx.py
@@ -0,0 +1,199 @@
+
+import glob
+import logging
+import os
+import re
+import subprocess
+
+import nimp.sys.platform
+
+class XSX(nimp.sys.platform.Platform):
+    ''' XSX platform description '''
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'xsx'
+        self.is_microsoft = True
+
+        self.layout_file_extension = 'xml'
+        if not os.getenv('UE_SDKS_ROOT'):
+            self.package_tool_path = os.path.join(os.getenv('GameDK', default='.'), 'bin', 'MakePkg.exe')
+
+        self.unreal_name = 'XSX'
+        self.unreal_config_name = 'XSX'
+        self.unreal_cook_name = 'XSX'
+        self.ue4_package_directory = '{uproject_dir}/Binaries/XSX'
+
+    def install_package(self, package_directory, env):
+        xvcs = glob.glob(package_directory + '/*.xvc')
+        if not xvcs:
+            raise RuntimeError('No xvc file in ' + package_directory)
+        if len(xvcs) != 1:
+            logging.info('Found xvcs:')
+            for xvc in xvcs:
+                logging.info('\t\t' + xvc)
+            raise RuntimeError('Multiple xvc files in ' + package_directory)
+
+        args = [ 'install', xvcs[0] ]
+        if env.device:
+            args.append('/X:' + env.device)
+        return XSX.xbapp(args, env.dry_run)
+
+    def launch_package(self, package_name, env):
+        if not package_name:
+            package_name = self.get_package_name_from_ini(env.uproject_dir, env.variant)
+
+        installed_packages = self.get_installed_packages(env.device)
+        package_name = self.pick_package(installed_packages, package_name, env.ue4_config)
+
+        args = [ 'launch', package_name ]
+        if env.device:
+            args.append('/X:' + env.device)
+        return XSX.xbapp(args, env.dry_run)
+
+    _PACKAGE_NAME_RE = re.compile(r'        (\S+)$')
+    def get_installed_packages(self, device_ip):
+        cmdline = '"' + XSX.XBAPP + '" list'
+        if device_ip:
+            cmdline += ' /x:' + device_ip
+        status, output = subprocess.getstatusoutput(cmdline)
+        if status != 0:
+            raise RuntimeError('Failed to get list of packages')
+        
+        installed_packages = []
+        for line in output.split('\n'):
+            m = XSX._PACKAGE_NAME_RE.match(line)
+            if not m:
+                continue
+            installed_packages.append(m.group(1))
+
+        return installed_packages
+ 
+    def pick_package(self, installed_packages, package_name, configuration):
+        matching_packages = []
+        for candidate in installed_packages:
+            if not package_name in candidate:
+                continue
+            if not configuration in candidate:
+                continue
+            matching_packages.append(candidate)
+
+        if len(matching_packages) == 1:
+            return matching_packages[0]
+
+        logging.info('Installed packages:')
+        for pkg in installed_packages:
+            logging.info('\t\t' + pkg)
+        if not matching_packages:
+            raise RuntimeError('Package ' + package_name + ' not found for configuration ' + configuration)
+        else:
+            raise RuntimeError('Multiple packages found for ' + package_name + ' for configuration ' + configuration)
+
+    def get_package_name_from_ini(self, project_directory, variant):
+        if variant:
+            ini_file_path = '{project_directory}/Config/Variants/{variant}/DefaultGame.ini'.format(**locals())
+        else:
+            ini_file_path = '{project_directory}/Config/DefaultGame.ini'.format(**locals())
+        with open(ini_file_path) as ini_file:
+            ini_content = ini_file.read()
+        return re.search(r'ProjectName=(.*)', ini_content, re.MULTILINE).group(1)
+
+    def find_gdk():
+        # if os.getenv('UE_SDKS_ROOT'):
+        #     raise RuntimeError('You seem to be using AutoSDK, which is not supported yet')
+        gdk = os.getenv('GameDK')
+        if gdk:
+            return gdk
+        return 'C:\\Program Files (x86)\\Microsoft GDK'
+
+    GDK = find_gdk()
+    KIT_PROFILE_PATH = 'Externals\\XboxOne\\Profiles'
+    KIT_PROFILE_README = KIT_PROFILE_PATH + '\\readme.md'
+    KIT_XBCONFIG_RE = re.compile(r'\w+:\s*([a-zA-Z0-9_-]+)', flags=re.IGNORECASE)
+
+    XBAPP = GDK + '\\bin\\xbapp.exe'
+    XBCONFIG = GDK + '\\bin\\xbconfig.exe'
+    XBCONNECT = GDK + '\\bin\\xbconnect.exe'
+    XBREBOOT = GDK + '\\bin\\xbreboot.exe'
+    XBRUN = GDK + '\\bin\\xbrun.exe'
+    MAKEPKG = GDK + '\\bin\\MakePkg.exe'
+    SIDELOAD = GDK + '\\180702\\sideload'
+
+    @staticmethod
+    def xbapp(args, dry_run=False):
+        result = nimp.sys.process.call([XSX.XBAPP] + args, dry_run=dry_run)
+        return result == 0
+
+    @staticmethod
+    def xbconfig(args, dry_run=False):
+        result = nimp.sys.process.call([XSX.XBCONFIG] + args, dry_run=dry_run)
+        return result == 0
+
+    @staticmethod
+    def xbconnect(ip, dry_run=False):
+        result = nimp.sys.process.call([XSX.XBCONNECT, ip], dry_run=dry_run)
+        return result == 0
+
+    @staticmethod
+    def xbreboot(dry_run=False):
+        result = nimp.sys.process.call([XSX.XBREBOOT], dry_run=dry_run)
+        return result == 0
+
+    @staticmethod
+    def xbrun(args, dry_run=False):
+        result = nimp.sys.process.call([XSX.XBRUN] + args, dry_run=dry_run)
+        return result == 0
+
+    @staticmethod
+    def makepkg(args, dry_run=False):
+        result = nimp.sys.process.call([XSX.MAKEPKG] + args, dry_run=dry_run)
+        return result == 0
+
+    @staticmethod
+    def defaultkit_ip():
+        logging.info(XSX.XBCONNECT + ' /Q /B')
+        status, output = subprocess.getstatusoutput('"' + XSX.XBCONNECT + '" /Q /B')
+        if status != 0:
+            raise(ValueError('invalid xbox kit ip address'))
+
+        return output
+
+    @staticmethod
+    def kit_profile_fname(root_dir, consoleType, config):
+        profiles_dir = os.path.join(root_dir, XSX.KIT_PROFILE_PATH, consoleType)
+        if not os.path.isdir(profiles_dir):
+            logging.error('unknown xbox console type : ' + consoleType)
+            return None
+
+        profile_fname = os.path.join(profiles_dir, config + '.txt')
+        profile_fname = os.path.abspath(profile_fname)
+
+        if not os.path.isfile(profile_fname):
+            logging.error('unknown xbox kit profile : ' + profile_fname)
+            return None
+
+        return profile_fname
+
+    @staticmethod
+    def kit_console_type():
+        logging.info(XSX.XBCONFIG + ' ConsoleType')
+        status, output = subprocess.getstatusoutput('"' + XSX.XBCONFIG + '" ConsoleType')
+        if status == 0:
+            output = output.strip()
+            m = XSX.KIT_XBCONFIG_RE.match(output)
+            if m:
+                return str(m.group(1))
+
+        raise(ValueError('invalid xbox kit console type'))
+
+    @staticmethod
+    def kit_console_name(kit):
+        logging.info(XSX.XBCONFIG + ' hostname /X ' + kit)
+        status, output = subprocess.getstatusoutput('"' + XSX.XBCONFIG + '" hostname /X ' + kit)
+        if status == 0:
+            output = output.strip()
+            m = XSX.KIT_XBCONFIG_RE.match(output)
+            if m:
+                return str(m.group(1))
+
+        raise (ValueError('invalid xbox kit console type'))
