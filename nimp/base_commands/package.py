04ef5af62a3fcb77cf59025f8912d0c67e4da84c
Laurent Cahour <laurent.cahour@dont-nod.com>
2021-08-31 12:42:49 +0200

    Set updcompat to 3 for all xbox one projects

--- nimp/base_commands/package.py
+++ nimp/base_commands/package.py
@@ -987,6 +987,7 @@ class Package(nimp.command.Command):
             package_command += [ '/productid', package_configuration.xbox_product_id ]
             package_command += [ '/contentid', package_configuration.xbox_content_id ]
             package_command += [ '/lt' ] if package_configuration.is_final_submission else []
+            package_command += [ '/updcompat', '3' ]
 
             if package_configuration.package_type in [ 'application', 'application_patch' ]:
                 package_command += [ '/genappdata', '/gameos', source + '/era.xvd' ]
