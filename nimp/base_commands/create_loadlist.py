e6e00e0e7315ce7760ea5f17213650e6890c79da
Laurent Cahour <laurent.cahour@dont-nod.com>
2022-07-01 12:59:42 +0200

    CreateLoadlist: implement --check-empty flag
    Returns check-mepty test in json format
    This is the first step towards a fully fledge command like
    nimp loadlist create / nimp loadlist list ...

--- nimp/base_commands/create_loadlist.py
+++ nimp/base_commands/create_loadlist.py
@@ -19,7 +19,7 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+import json
 import os
 import re
 
@@ -32,6 +32,7 @@ class CreateLoadlist(nimp.command.Command):
 		parser.add_argument('changelists', nargs = '+', help = 'select the changelists to list files from')
 		parser.add_argument('-o', '--output', help = 'output file')
 		parser.add_argument('-e', '--extensions', nargs = '*', help = 'file extensions to include', default = [ 'uasset', 'umap' ])
+		parser.add_argument('--check-empty', action = 'store_true', help = 'Returns check empty in json format')
 		nimp.utils.p4.add_arguments(parser)
 		return True
 
@@ -60,6 +61,9 @@ class CreateLoadlist(nimp.command.Command):
 		loadlist_path = env.output if env.output else f'{env.unreal_loadlist}'
 		loadlist_path = os.path.abspath(env.format(nimp.system.sanitize_path(loadlist_path)))
 
+		if env.check_empty:
+			return self.check_empty_loadlist(modified_files)
+
 		with open(env.format(loadlist_path), 'w+') as output:
 			lines = output.readlines()
 			for file in modified_files:
@@ -67,4 +71,15 @@ class CreateLoadlist(nimp.command.Command):
 					print(file)
 					output.write(f'{file}\n')
 
+		return True
+
+	def check_empty_loadlist(self, modified_files):
+		results = {'loadlist_is_empty': True}
+		if modified_files:
+			results['loadlist_is_empty'] = False
+
+		json_content = json.dumps(results, indent=4)
+		print('<loadlist_start>')
+		print(json_content)
+		print('<loadlist_end>')
 		return True
