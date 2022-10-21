c1b05529d7d7766fa1f132b3604467c726078485
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2022-10-21 13:49:11 +0200

    command run: fix subprocess.call do not have a dry_run arg

--- nimp/base_commands/run.py
+++ nimp/base_commands/run.py
@@ -208,7 +208,7 @@ class ConsoleGameCommand(nimp.command.Command):
         logging.info('Running "%s"', ' '.join(cmdline))
         if env.dry_run:
             return True
-        result = subprocess.call(cmdline, dry_run=env.dry_run) # Call subprocess directly to allow "dynamic" output (with progress percentage)
+        result = subprocess.call(cmdline) # Call subprocess directly to allow "dynamic" output (with progress percentage)
         return result <= 1 # 0: nothing to copy. 1: some files were copied
     
     def fetch_with_copy(self, env):
