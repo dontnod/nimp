cd9a8104666fce442fbc713ff0a787b9372b2a05
Thibault Jochem <thibault.jochem@dont-nod.com>
2022-12-15 16:43:47 +0100

    Fix string formatting in logging.info

--- nimp/base_commands/package.py
+++ nimp/base_commands/package.py
@@ -1011,7 +1011,7 @@ class Package(nimp.command.Command):
         if not os.path.exists(src_microsoft_game_config):
             raise FileNotFoundError(f'Missing file: {src_microsoft_game_config}, handcrafted MicrosoftGame.config for the DLC is required to exist.')
         dst_microsoft_game_config = env.format('{uproject_dir}/Saved/StagedBuilds/XSX/Manifest/{unreal_config}/MicrosoftGame.config')
-        logging.info(f'Copying {src_microsoft_game_config} to {dst_microsoft_game_config}')
+        logging.info(f'Copying %s to %s', src_microsoft_game_config, dst_microsoft_game_config)
         shutil.copyfile(src_microsoft_game_config, dst_microsoft_game_config)
 
     @staticmethod
